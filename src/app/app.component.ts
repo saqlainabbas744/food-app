608b8bca7c20a4d4a6c1ab8091a41fd0f56380a3
Saqlain Abbas <saqlainabbas744@gmail.com>
2024-06-13 06:27:05 -0700

    Add food list page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FoodListComponent } from './food-list/food-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [FoodListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
